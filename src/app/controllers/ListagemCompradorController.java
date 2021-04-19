7a1b019ce740faf8c43599a69a526fd6e9821d8e
Wadson Pontes <wadsonpontesdesouza@hotmail.com>
2021-04-19 20:13:54 -0300

    Listagem de Compradores

--- src/app/controllers/ListagemCompradorController.java
+++ src/app/controllers/ListagemCompradorController.java
@@ -1,41 +1,40 @@
 package app.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import app.models.pessoa.Comprador;
 import app.utils.CenaUtil;
 
-public class ListagemCompradorController {
+public class ListagemCompradorController implements Initializable {
+	private ObservableList<Comprador> compradores;
+	
 	@FXML
     private TextField campoBusca;
 
     @FXML
     private ListView<Comprador> listagem;
 	
-	
 	@FXML
     private Button botaoVoltar;
-	
-	@FXML
-    void botaoCompradorClicado(MouseEvent event) {
-		CenaUtil.trocarCena(botaoComprador, getClass(), "/app/views/TelaListagemComprador.fxml");
-	}
-	
-	@FXML
-    void botaoVendedorClicado(MouseEvent event) {
-		CenaUtil.trocarCena(botaoVendedor, getClass(), "/app/views/TelaListagemVendedor.fxml");
-	}
-	
-	@FXML
-    void botaoProdutoClicado(MouseEvent event) {
-		CenaUtil.trocarCena(botaoProduto, getClass(), "/app/views/TelaListagemProduto.fxml");
-	}
 
     @FXML
     void botaoVoltarClicado(MouseEvent event) {
-    	CenaUtil.trocarCena(botaoVoltar, getClass(), "/app/views/TelaMenuPrincipal.fxml");
+    	CenaUtil.trocarCena(botaoVoltar, getClass(), "/app/views/TelaListagem.fxml");
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		compradores = FXCollections.observableArrayList(Sistema.compradores);
+		listagem.setItems(compradores);
 	}
 }
